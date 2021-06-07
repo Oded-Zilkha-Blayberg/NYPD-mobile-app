4174e6fbe11be6391bf5df89848e22deef69be03
hackathon IDF <hack@cs.colman.ac.il>
2021-06-07 17:50:42 +0300

    navbar changes

--- components/Basepage.js
+++ components/Basepage.js
@@ -3,6 +3,7 @@ import Navbar from "./Navbar";
 import { Icon } from 'react-native-elements';
 import { Divider } from 'react-native-elements';
 import { StyleSheet, View } from 'react-native';
+import "@fontsource/heebo";
 
 
 const styles = StyleSheet.create({
@@ -14,19 +15,23 @@ const styles = StyleSheet.create({
         color:"white",
         position:"absolute",
         width:"100%",
-        height:"16%",
+        height:"12%",
         top:"0%",
         borderBottomLeftRadius: 20,
         borderBottomRightRadius: 20,
-        fontFamily:"Heebo-VariableFont"
+        fontFamily:"heebo",
+        fontSize: 25,
     },
+    title: {
+        padding: 20
+    }
 });
 
 export default function Basepage(props) {
     return (
         <React.Fragment>
             <Divider style={styles.titleContainer}>
-                <Icon name={props.icon} color="white" />
+                <Icon name={props.icon} size="40px" color="white" iconStyle={styles.title}/>
                 <h3>{props.title}</h3>
             </Divider>
             <View>
