91d89751bd6c47ef43309d182f10091ba9312718
hackathon IDF <hack@cs.colman.ac.il>
2021-06-08 19:18:44 +0300

    feat(map): added functional map

--- views/MapScreen.js
+++ views/MapScreen.js
@@ -11,6 +11,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { WebView } from 'react-native-webview';
 
 
 const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
@@ -18,10 +19,15 @@ const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      // backgroundColor: '#000000',
       alignItems: 'center',
       justifyContent: 'center',
       writingDirection: 'ltr',
+      
+      // left: 0,
+      // right: 0,
+      // top: 0,
+      // bottom: 0
     },
   });
 
@@ -32,10 +38,32 @@ const styles = StyleSheet.create({
 
               {/* <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" /> */}
 
-            <Basepage title="מפה " icon="map" navigation={navigation}>
-                <Text>This is map</Text>               
-            </Basepage>
+
+              {/* <iframe src="http://map3-service-tmzmap3.apps.openforce.openforce.biz/#/icontrolmobile" /> */}
+          <WebView
+          // scalesPageToFit={true}
+          // bounces={false}
+          // javaScriptEnabled
+          style={{ height: 800, width: 400 }}
+          source={{ uri: 'http://map3-service-tmzmap3.apps.openforce.openforce.biz/#/icontrolmobile'}}
+          // {{
+          //   html: `
+          //         <!DOCTYPE html>
+          //         <html>
+          //           <head></head> 
+          //           <body>
+          //             <div id="baseDiv">'<iframe src="http://map3-service-tmzmap3.apps.openforce.openforce.biz/#/icontrolmobile" style="width:1000, height:1000"/>'</div> //<--- add your iframe here
+          //           </body>
+          //         </html>
+          //   `,
+          // }}
+          // automaticallyAdjustContentInsets={false}
+        />
+
+
+            <Navbar navigation ={navigation}></Navbar>
             <StatusBar style="auto" />
+
           </View>
       </StylesProvider>
     );
